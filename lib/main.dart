943e64faaf8463624248214cf067868e1aac6883
developerSajib88 <developersajib88@gmail.com>
2024-11-29 15:16:28 +0600

    Zego Cloud Android Configured

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:zego_uikit_prebuilt_call/zego_uikit_prebuilt_call.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: CallPage(),
+    );
+  }
+}
+
+
+class CallPage extends StatelessWidget {
+  const CallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ZegoUIKitPrebuiltCall(
+      appID: 0123, // Fill in the appID that you get from ZEGOCLOUD Admin Console.
+      appSign: "", // Fill in the appSign that you get from ZEGOCLOUD Admin Console.
+      userID: 'user_id',
+      userName: 'user_name',
+      callID: "callID",
+      // You can also use groupVideo/groupVoice/oneOnOneVoice to make more types of calls.
+      config: ZegoUIKitPrebuiltCallConfig.oneOnOneVideoCall(),
+    );
+  }
+}
+
+
